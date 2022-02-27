e8ee353a37f712988a30efd076aa890cdea61795
coderowson <runurowson@gmail.com>
2022-02-27 09:25:50 +0600

    first commit

---
+++ problem.js
@@ -0,0 +1,15 @@
+const mySelf = {
+    fullname: 'Rowshon Ara',
+    married: false,
+    age: 27,
+    location ={
+        hometown: 'Khulna',
+        presentAddress: 'Daulatpur',
+        permanebtAddress: 'Bejerdanga',
+    },
+    const connection = () => {
+
+    }
+
+
+}
