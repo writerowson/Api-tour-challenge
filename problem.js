d5a6977e218f5054206bac81fb5fd9992aef4276
coderowson <runurowson@gmail.com>
2022-03-14 13:33:10 +0600

    problem 03

--- problem.js
+++ problem.js
@@ -1,15 +1,22 @@
+// ১. একটা অবজেক্ট ডিক্লেয়ার করো। সেই অবজেক্ট এর মিনিমাম পাঁচটা প্রপার্টি থাকবে। তারপর একটা প্রপার্টি এর মান হবে অন্য আরেকটা অবজেক্ট , আরেকটা প্রপার্টি এর মান হবে একটা array । একটা প্রপার্টি এর মান হবে একটা ফাংশন(মেথড) । এই মেথড কে কল করলে সে মেথড এর ভিতর থেকে অবজেক্ট এর যেকোন একটা প্রপার্টি রিটার্ন করবে। আর বাকি দুইটা তোমার ইচ্ছা মতো। 
+
 const mySelf = {
     fullname: 'Rowshon Ara',
-    married: false,
-    age: 27,
-    location ={
+    grauated: true,
+    mobile: 086574001,
+    money: 30,
+    location: {
         hometown: 'Khulna',
         presentAddress: 'Daulatpur',
         permanebtAddress: 'Bejerdanga',
     },
-    const connection = () => {
-
+    Skill: ['Blogging', 'graphics Design', 'Content writing'],
+    connection: function (name) {
+        this.qualification = this.grauated + name
+        return this.qualification
     }
 
-
 }
+console.log()
+
+// ২. একটা টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার করো। সেটার মধ্যে উপরের অবজেক্ট এর একটা তিনটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। এর মধ্যে একটা প্রপার্টি সেট করবে--যেটা নেস্টেড অবজেক্ট আছে সেটার প্রপার্টি থেকে। আরেকটা প্রপার্টি হবে যে প্রপার্টি এর মান একটা array সেটার সেকেন্ড পজিশন এর উপাদান দিয়ে। আর বাকি একটা তোমার ইচ্ছা মতো বসাতে পারো। 
